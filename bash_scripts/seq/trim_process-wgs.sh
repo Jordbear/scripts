5cb4a9c16f221f025ab795eee2e8c9c2f674ba68
Jordbear <jordan.brown996@gmail.com>
2020-02-21 10:29:55 +0000

    created trim_process-wgs.sh and updated process-wgs.sh with parameter printing

--- bash_scripts/seq/trim_process-wgs.sh
+++ bash_scripts/seq/trim_process-wgs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+adapters=TruSeq3-PE.fa
+echo Trimming adapters: $adapters
 ref_index=/mnt/e/ref_bacteria/T/E_coli_tfs/bowtie2_tfs/tfs
 echo Aligning to: ${ref_index}
 ref=/mnt/e/ref_bacteria/T/E_coli_tfs/e_coli_tfs.fasta
@@ -8,6 +10,18 @@ echo ''
 
 
 
+mkdir trimmed
+for fq1 in *1.fastq.gz; do
+  fq2=${fq1%%1.fastq.gz}'2.fastq.gz'
+  echo $fq1
+  echo $fq2
+  java -jar /mnt/e/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 4 -phred33 $fq1 $fq2 \
+  trimmed/${fq1%%1.fastq.gz}'trimmed_1.fastq.gz' trimmed/${fq1%%1.fastq.gz}'trimmed_1U.fastq.gz' trimmed/${fq2%%2.fastq.gz}'trimmed_2.fastq.gz' trimmed/${fq2%%2.fastq.gz}'trimmed_2U.fastq.gz' \
+  ILLUMINACLIP:/mnt/e/Trimmomatic-0.39/adapters/$adapters:2:30:10:1:true LEADING:30 TRAILING:30 MINLEN:30
+done
+
+
+cd trimmed
 mkdir bams
 echo Aligning read pairs to reference: ${ref_index}
 for fq1 in *1.fastq.gz; do
