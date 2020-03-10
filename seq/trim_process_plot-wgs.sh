8e59f7a9e484569ea0fd4e9d2d1a94c221757c17
Jordbear <jordan.brown996@gmail.com>
2020-03-10 10:30:57 +0000

    Further updates to jbscripts2. Replaced jbscripts with jbscripts2. Fixed bug with trim_process scripts.

--- seq/trim_process_plot-wgs.sh
+++ seq/trim_process_plot-wgs.sh
@@ -17,7 +17,7 @@ for fq1 in *1.fastq.gz; do
   echo $fq2
   java -jar $TRIMMOMATIC PE -threads 4 -phred33 $fq1 $fq2 \
   trimmed/${fq1%%1.fastq.gz}'trimmed_1.fastq.gz' trimmed/${fq1%%1.fastq.gz}'trimmed_1U.fastq.gz' trimmed/${fq2%%2.fastq.gz}'trimmed_2.fastq.gz' trimmed/${fq2%%2.fastq.gz}'trimmed_2U.fastq.gz' \
-  ILLUMINACLIP:${TRIMMOMATIC%%trimmomatic-0.39.jar}/adapters/${adapters}:2:30:10:1:true LEADING:30 TRAILING:30 MINLEN:30
+  ILLUMINACLIP:${TRIMMOMATIC%%trimmomatic-0.39.jar}adapters/${adapters}:2:30:10:1:true LEADING:30 TRAILING:30 MINLEN:30
 done
 
 
