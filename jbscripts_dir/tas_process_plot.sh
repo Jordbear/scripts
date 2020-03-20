0586e12c6e7ed4d86aa20782837533504482dadd
Jordbear <jordan.brown996@gmail.com>
2020-03-20 10:44:56 +0000

    Changed directory naming convention from "bams" to "aligned".

--- jbscripts_dir/tas_process_plot.sh
+++ jbscripts_dir/tas_process_plot.sh
@@ -29,15 +29,15 @@ fi
 
 
 
-mkdir bams
+mkdir aligned
 for fq1 in *1.fastq.gz; do
   fq2=${fq1%%1.fastq.gz}'2.fastq.gz'
   echo $fq1
   echo $fq2
   echo ${fq1%%_1.fastq.gz}'.bam'
-  bowtie2 -q -p 4 -X 1500 -x $ref_index -1 $fq1 -2 $fq2 | samtools sort -O BAM > bams/${fq1%%_1.fastq.gz}'.bam'
+  bowtie2 -q -p 4 -X 1500 -x $ref_index -1 $fq1 -2 $fq2 | samtools sort -O BAM > aligned/${fq1%%_1.fastq.gz}'.bam'
 done
-cd bams
+cd aligned
 
 
 mkdir dmarked
