846d56cfa001e7e5410124952220016c726cfb66
Jordan Brown <jordanbrown@Jordans-MacBook-Pro.local>
2020-07-20 10:38:40 +0100

    Updated picolog script

--- python_scripts/picolog.py
+++ python_scripts/picolog.py
@@ -7,42 +7,48 @@ import glob
 import seaborn as sns
 sns.set_style('white', {'axes.grid': True, 'xtick.bottom': True, 'ytick.left': True})
 sns.set_context(rc={'patch.linewidth': '0.0'})
-sns.set_palette(sns.color_palette(['#ce0e2d', '#005cb9', '#f5a800', '#45c2b1', '#035c67']))
-
-all_log = glob.glob('*.csv')
+sns.set_palette(sns.color_palette(['#28215D', '#706F6F', '#DE6752', '#DC4B72']))
 
 
-names = []
-for i in all_log:
-        names.append(i)
-namesx = [i.replace('.csv', '') for i in names]
-print(namesx)
+
+files = sorted(glob.glob('*.csv'))
+print(files)
 print('')
 
-dfm = list(pd.read_csv(f, sep=',', header=None, skiprows=1) for f in all_log)
+names = [i.replace('.csv', '') for i in files]
+print(names)
+print('')
 
-samno = 0
-for i in dfm:
-    i['Sample']=namesx[samno]
-    samno+=1
+dfl = [pd.read_csv(f, sep=',', skiprows=0, index_col=False) for f in files]
 
-dfm = pd.concat(dfm, axis=0, sort=True)
+group = ['Cycler1', 'Cycler2']
+count = 0
+for i in dfl:
+    i['sample'] = names[count]
+    print(dfl[count])
+    print('')
+    i['group'] = group[count]
+    print((dfl[count]).head())
+    print('')
+    dfl[count] = dfl[count].melt(id_vars=['Unnamed: 0', 'sample', 'group'])
+    print(dfl[count])
+    print('')
+    count+=1
 
-dfm.columns=['Time (seconds)', 'Temperature (celsius)', 'Sample']
+dfc = pd.concat(dfl, axis=0, sort=True)
+print(head(dfc))
+print('')
 
 
-
-plot = sns.lineplot(x='Time (seconds)', y='Temperature (celsius)', hue='Sample', data=dfm)
-plot.legend(loc='center right', bbox_to_anchor=(1.25, 0.5), framealpha=1)
+fig = plt.figure(figsize=(7, 5))
+ax = fig.add_subplot(1, 1, 1)
+plot = sns.lineplot(x='time', y='value', hue='variable', units='sample', estimator=None, data=dfc)
+handles, labels = plot.get_legend_handles_labels()
+plot.legend(loc='center right', bbox_to_anchor=(1.2, 0.5), framealpha=1, title='Position', handles=handles[1:], labels=labels[1:])
 plot.set(xlim=(0, None))
-
-plot.figure.savefig('thermal_profile_OldVsNew_plusv4.png', format='png', dpi=1000, bbox_inches='tight')
-
-print(dfm)
-print('')
-print(dfm.head())
-print('')
+plot.set(xlabel='Time (s)', ylabel='Temperature (\u00B0C)')
+plot.figure.savefig('plot.png', format='png', dpi=1000, bbox_inches='tight')
 
 
 
-print('Finish')
+print('finished')
