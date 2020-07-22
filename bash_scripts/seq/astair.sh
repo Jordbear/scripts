1c6930a5cabc40d94eca33f6a165affca3eb1673
Jordbear <jordan.brown996@gmail.com>
2020-07-22 16:31:32 +0100

    added taps_conversion.py and modified astair.sh to give outputs directory specific to rname

--- bash_scripts/seq/astair.sh
+++ bash_scripts/seq/astair.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ref=/Users/jordanbrown/sequencing/reference_genomes/spikes/J02459.1.fa
-mkdir astair
+ref=/Users/jordanbrown/sequencing/reference_genomes/spikes/synthetic_N5mCNN.fa
+rname=synthetic_N5mCNN
+mkdir "astair-${rname}"
 for i in *.bam; do
   echo $i
-  astair call -i $i -f $ref -d 'astair/'
+  astair call -i $i -f $ref -d "astair-${rname}/"
 done
