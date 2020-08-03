e9ba6aacdf1687a878f6c0a191d69c9525c6d8a9
Jordbear <jordan.brown996@gmail.com>
2020-08-03 11:48:59 +0100

    updated WGS_intervals to accomodate different intervals in the same folder

--- bash_scripts/seq/wgsmetrics_interval.sh
+++ bash_scripts/seq/wgsmetrics_interval.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 mkdir coverage
 ref=/Users/jordanbrown/sequencing/reference_genomes/mm9/mm9-new_spike/mm9-new_spike.fa
-annotation=/Users/jordanbrown/sequencing/reference_genomes/mm9/mm9.bed
-interval=mm9
+annotation=/Users/jordanbrown/sequencing/reference_genomes/spikes/J02459.1.bed
+interval=J02459.1
 files=(*.bam)
 
 java -jar $PICARD BedToIntervalList \
 I=$annotation \
-O=coverage/interval_list.tsv \
+O=coverage/interval_list-$interval.tsv \
 SD=${files[0]}
 
 for bam in *bam; do
@@ -18,5 +18,5 @@ for bam in *bam; do
   I=$bam \
   O=coverage/${bam%%.bam}"_wgs-$interval.tsv" \
   R=$ref \
-  INTERVALS=coverage/interval_list.tsv
+  INTERVALS=coverage/interval_list-$interval.tsv
 done
