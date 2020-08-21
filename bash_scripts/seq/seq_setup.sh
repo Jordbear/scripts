6fa640c31a644031a4047cad6e9ff65365ab13ff
Jordbear <jordan.brown996@gmail.com>
2020-08-21 13:16:21 +0100

    added bedtools install to setup script

--- bash_scripts/seq/seq_setup.sh
+++ bash_scripts/seq/seq_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 trimmomatic_link=http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip
 picard_link=https://github.com/broadinstitute/picard/releases/download/2.23.3/picard.jar
+bedtools_link=https://github.com/arq5x/bedtools2/releases/download/v2.29.2/bedtools.static.binary
+
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
@@ -25,6 +27,10 @@ sudo apt-get -y install samtools
 wget $picard_link
 picard_jar=`pwd`'/picard.jar'
 echo 'PICARD='$picard_jar >> ~/.bashrc
+
+wget $bedtools_link
+chmod u+x bedtools.static.binary
+mv bedtools.static.binary ~/.local/bin/bedtools
 
 sudo apt-get -y install python3-pip
 pip3 install astair
