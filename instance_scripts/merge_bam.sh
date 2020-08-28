92fcea8e3ed24d72b9ceb79d094c0363c1ab5c55
Jordbear <jordan.brown996@gmail.com>
2020-08-28 09:12:56 +0100

    reverted merge and changed dir name

---
+++ instance_scripts/merge_bam.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir merged
+for bam in *L001*.bam; do
+  echo $bam
+  echo ${bam%%_L001*}${bam##*L001}
+  bams=`find . -name "${bam%%_L001*}*${bam##*L001}" -print0 | sort -z`
+  echo $bams
+  input=${bams//.\// I=}
+  input=${input# }
+  echo $input
+  java -jar $PICARD MergeSamFiles $input O=merged/${bam%%_L001*}${bam##*L001} USE_JDK_DEFLATER=true USE_JDK_INFLATER=true USE_THREADING=true
+done
