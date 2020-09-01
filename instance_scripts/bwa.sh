329188806cc31cf05aa77b3ce0ce5a8052394a73
Jordbear <jordan.brown996@gmail.com>
2020-09-01 12:19:01 +0100

    Changed bwa script to work off of trimgalore output files

--- instance_scripts/bwa.sh
+++ instance_scripts/bwa.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-ref_index=/data/references/mm10-base_spikes/bwa_index/mm10-base_spikes.fa
+ref_index=/data/references/GRCh38-base_spikes/bwa_index/Homo_sapiens.GRCh38.dna_sm.primary_assembly-base_spikes.fa
 threads=`nproc --all`
 echo Aligning read pairs to reference index: ${ref_index}
 mkdir aligned
 
 for fq1 in *R1*.fq.gz; do
   fq2=${fq1/R1/R2}
-  bam=${fq1/R1_/}
+  fq2=${fq2/val_1/val_2}
+  bam=${fq1/_R1*val_1/}
   bam=${bam%%.fq.gz}'.bam'
   echo $fq1
   echo $fq2
