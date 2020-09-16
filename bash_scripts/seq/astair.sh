6e2ef65bf9649fd2753382d2665609c65304b924
Jordbear <jordan.brown996@gmail.com>
2020-09-16 15:32:11 +0100

    parallelised astair script

--- bash_scripts/seq/astair.sh
+++ bash_scripts/seq/astair.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-ref=/Users/jordanbrown/sequencing/reference_genomes/spikes/unmodified_2kb.fa
-rname=unmodified_2kb
+ref=/Users/jordanbrown/sequencing/references/
+rname=test
 mkdir "astair-${rname}"
 for i in *.bam; do
   echo $i
-  astair call -i $i -f $ref -d "astair-${rname}/" -md 10000
+  astair call -i $i -f $ref -d "astair-${rname}/" -md 10000 &
+  ntasks=`jobs -p | wc -w`
+  echo $ntasks
+  if [ $ntasks -ge 6 ]; then wait; fi
 done
