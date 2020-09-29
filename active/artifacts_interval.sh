d8cf3c925aa95f22cf448d61909618daca040ade
Jordbear <jordan.brown996@gmail.com>
2020-09-29 09:51:21 +0100

    fixed paths

--- active/artifacts_interval.sh
+++ active/artifacts_interval.sh
@@ -3,12 +3,12 @@ ref=/Users/jordanbrown/sequencing/references/GRCh38-base_spikes/Homo_sapiens.GRC
 annotation=/Users/jordanbrown/sequencing/references/GRCh38/Homo_sapiens.GRCh38.dna_sm.primary_assembly.bed
 interval=GRCh38
 
-mkdir artifacts2
+mkdir artifacts
 files=(*.bam)
 
 java -jar $PICARD BedToIntervalList \
 I=$annotation \
-O=artifacts2/$interval.interval_list \
+O=artifacts/$interval.interval_list \
 SD=${files[0]}
 
 for bam in *bam; do
@@ -17,7 +17,7 @@ for bam in *bam; do
   echo $ref
   java -jar $PICARD CollectSequencingArtifactMetrics \
   I=$bam \
-  O=artifacts2/${bam%%.bam}"_artifacts-$interval" \
+  O=artifacts/${bam%%.bam}"_artifacts-$interval" \
   R=$ref \
   INTERVALS=artifacts/$interval.interval_list &
   ntasks=`jobs -p | wc -w`
