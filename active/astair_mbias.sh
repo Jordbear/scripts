8cb4a75eebefb3340d21a28333cfa2739a5f476c
Jordbear <jordan.brown996@gmail.com>
2020-11-05 10:43:42 +0000

    fixes

--- active/astair_mbias.sh
+++ active/astair_mbias.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-ref=/Users/jordanbrown/sequencing/references/GRCh38-base_spikes_pUC19/Homo_sapiens.GRCh38.dna_sm.primary_assembly-base_spikes_pUC19.fa
+ref=/Users/jordanbrown/sequencing/references/GRCh38-base_spikes_pUC19/Homo_sapiens.GRCh38.dna_sm.primary_assembly-base_spikes.fa
 rname=J02459.1
 mkdir "astair_mbias-${rname}"
-for i in S20*.bam; do
+for i in *.bam; do
   echo $i
-  astair mbias -i $i -f $ref -d "astair_mbias-${rname}/" --method CtoT --read_length 150 -chr $rname --plot &
+  astair mbias -i $i -f $ref -d "astair_mbias-${rname}/" --method mCtoT --read_length 150 -chr $rname --plot &
   ntasks=`jobs -p | wc -w`
   echo $ntasks
   if [ $ntasks -ge 3 ]; then wait; fi
