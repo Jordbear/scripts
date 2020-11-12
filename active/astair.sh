9dc91a52b8e7d7998f8057468d935a6b2900f8be
Jordbear <jordan.brown996@gmail.com>
2020-11-12 13:00:40 +0000

    added zero coverage flag

--- active/astair.sh
+++ active/astair.sh
@@ -4,7 +4,7 @@ rname=J02459.1
 mkdir "astair-${rname}"
 for i in *.bam; do
   echo $i
-  astair call -i $i -f $ref -d "astair-${rname}/" -md 10000 --minimum_base_quality 13 &
+  astair call -i $i -f $ref -d "astair-${rname}/" -md 10000 -zc --minimum_base_quality 13 &
   ntasks=`jobs -p | wc -w`
   echo $ntasks
   if [ $ntasks -ge 3 ]; then wait; fi
