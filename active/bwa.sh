e04bb7c7f6d26b8ef99b147220c0fe2c9fe7950e
Ubuntu <ubuntu@ip-172-31-4-174.eu-west-2.compute.internal>
2020-11-12 14:28:30 +0000

    file detection changes

--- active/bwa.sh
+++ active/bwa.sh
@@ -5,10 +5,12 @@ threads=`nproc --all`
 echo Aligning read pairs to reference index: ${ref_index}
 mkdir aligned
 
-for fq1 in *R1*.f???q.gz; do
+for fq1 in *R1*.f*q.gz; do
   fq2=${fq1/R1/R2}
+  fq2=${fq2/_val_1/_val_2}
   bam=${fq1/_R1/}
-  bam=${bam%%.f???q.gz}'.bam'
+  bam=${bam/_val_1/}
+  bam=${bam%%.f*q.gz}'.bam'
   echo $fq1
   echo $fq2
   echo $bam
