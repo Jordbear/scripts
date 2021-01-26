8f28e0498bdcfcd37ee590292fc905576218ec79
Jordbear <jordan.brown996@gmail.com>
2021-01-26 12:19:36 +0000

    tidied script

--- active/gc_bias.py
+++ active/gc_bias.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 11:58:43 2021
+
+@author: jordanbrown
+"""
+
 from matplotlib import pyplot as plt
 from matplotlib import ticker as ticker
 import pandas as pd
@@ -10,31 +18,33 @@ sns.set_style('white', {'axes.grid': True, 'xtick.bottom': True, 'ytick.left': F
 sns.set_context(rc={'patch.linewidth': '0.0'})
 sns.set_palette(sns.color_palette('muted'))
 
-
-
+### get list of files
 files = sorted(glob.glob('*_gc.tsv'))
 print(files)
 print('')
 
-
+### get sample names from file names
 names = [i.rsplit('_S', 1)[0] for i in files]
 print(names)
 print('')
 
-conditions = [i[4:6] for i in names]
-print(conditions)
-print('')
-
+### get sample numbers from file names
 number = [i.split('_S')[-1] for i in files]
-number = [i.replace('_001_gc.tsv', '') for i in number]
+number = [i.replace('_gc.tsv', '') for i in number]
 number = ['0'+i for i in number]
 number = [i[-2:] for i in number]
 print(number)
 print('')
 
+### get conditions
+conditions = [i[4:9] for i in names]
+print(conditions)
+print('')
 
+### read in list of dataframes
 dfl = [pd.read_csv(i, sep='\t', skiprows=6) for i in files]
 
+### add sample information to dataframes individually
 count = 0
 for i in dfl:
     i['sample'] = names[count]
@@ -45,49 +55,29 @@ for i in dfl:
     print(dfl[count])
     count+=1
 
-# box = ['AND', 'AND', 'AND', 'AND', 'AND', 'AND', 'AND', 'CAP', 'CAP', 'CAP', 'CAP', 'CAP', 'CAP', 'CAP', 'CAP', 'CAP', 'BET', 'BET', 'BET', 'CAP', 'CAP', 'Tube', 'Tube']
-# count = 0
-# for i in dfl:
-#     i['box'] = box[count]
-#     print(dfl[count])
-#     count+=1
-# count = 0
-
-# pos = ['p5', 'p6', 'p7', 'p5', 'p6', 'p7', 'p5', 'p5', 'p6', 'p5', 'p6', 'p7', 'p5', 'p5', 'p6', 'p7', 'p5', 'p6', 'p7', 'p6', 'p5/7', 'Tube', 'Tube']
-# count = 0
-# for i in dfl:
-#     i['pos'] = pos[count]
-#     print(dfl[count])
-#     count+=1
-# count = 0
-
+### concatenate dataframes
 dfc = pd.concat(dfl, axis=0)
 print(dfc)
 print('')
 
-dfe = dfl[0]
-print(dfe)
-print('')
-
-total_windows = dfe['WINDOWS'].sum()
+### get total number of windows
+total_windows = dfl[0]['WINDOWS'].sum()
 print(total_windows)
 print('')
 
-dfe['Proportion'] = dfe['WINDOWS']/total_windows
-print(dfe)
-print('')
+### sort values by numbers and create subset dataframes
+dfc = dfc.sort_values(['number'])
+# dfc1 = dfc[dfc['number'].isin(['01', '02', '03', '04', '05', '11'])]
+# dfc2 = dfc[dfc['number'].isin(['05', '06', '07', '08', '09', '10'])]
+# dfc3 = dfc[dfc['number'].isin(['11', '12', '13', '14', '15', '16', '17', '18'])]
 
-# dfc = dfc.sort_values(['number'])
-dfc1 = dfc[dfc['number'].isin(['01', '02', '03', '04', '05', '11'])]
-dfc2 = dfc[dfc['number'].isin(['05', '06', '07', '08', '09', '10'])]
-dfc3 = dfc[dfc['number'].isin(['11', '12', '13', '14', '15', '16', '17', '18'])]
-
+### create figure with twin axes
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 1, 1)
 ax2 = ax1.twinx()
 
-
-sns.barplot(x='GC', y='WINDOWS', color='#035c67', data=dfe, ax=ax1)
+### plot the histogram
+sns.barplot(x='GC', y='WINDOWS', color='#035c67', data=dfl[0], ax=ax1)
 ax1.set(xlim=(0, 100))
 ax1.xaxis.set_major_locator(ticker.MultipleLocator(10))
 ax1.xaxis.set_major_formatter(ticker.ScalarFormatter())
@@ -111,21 +101,16 @@ ax1.yaxis.set_label_position('right')
 ax1.set(xlabel='GC percentage of windows', ylabel='Number of windows')
 ax1.legend(loc='center right', bbox_to_anchor=(1.5, 0.5), framealpha=1).remove()
 
-
+### plot normalised coverage
 ax2.axes.axhline(1, color='grey', ls='--')
-sns.lineplot(x='GC', y='NORMALIZED_COVERAGE', hue='sample', units='sample', estimator=None, data=dfc, ax=ax2)
+sns.lineplot(x='GC', y='NORMALIZED_COVERAGE', hue='condition', units='sample', estimator=None, data=dfc, ax=ax2)
 ax2.set(xlim=(0, 100))
 ax2.set(ylim=(0, 2))
-# ax2.legend(loc='center right', bbox_to_anchor=(1.4, 0.5), framealpha=1).texts[0].set_text('Sample')
 ax2.legend(loc='center right', bbox_to_anchor=(1.55, 0.5), framealpha=1)#.remove()
 ax2.grid(False)
 ax2.yaxis.tick_left()
 ax2.yaxis.set_label_position('left')
 ax2.set(xlabel='GC percentage of windows', ylabel='Normalised coverage')
 
-
+### save figure
 plt.savefig('gc_bias.png', format='png', dpi=500, bbox_inches='tight')
-
-
-
-print('finished')
