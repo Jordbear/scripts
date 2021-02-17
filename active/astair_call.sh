0ea61656207dd26af06ce9bfd4ff0f7bf26c6e4b
Jordan Brown <jordan@ip-172-31-8-186.eu-west-2.compute.internal>
2021-02-17 11:37:11 +0000

    updated to process all together

--- active/astair_call.sh
+++ active/astair_call.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-ref=/Users/jordanbrown/sequencing/references/spikes/J02459.1.fa
-rname=J02459.1
+ref=/pipeline/references/lambda/J02459.1.fa
 nthreads=`nproc --all`
-mkdir "astair-${rname}"
+mkdir astair
 for i in *.bam; do
   echo $i
-  astair call -i $i -f $ref -d "astair-${rname}/" -md 10000 -zc --minimum_base_quality 13 &
+  astair call -i $i -f $ref -d astair -md 10000 -zc --minimum_base_quality 13 &
   ntasks=`jobs -p | wc -w`
   echo $ntasks
   if [ $ntasks -ge $nthreads ]; then wait; fi
